fa3a6de3278e037b68bbe3dfc97e8747977cb276
Clayton Dewey <clay@dryadmedia.com>
2023-01-14 13:45:46 -0600

    Import password generator components

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,12 @@
 import '../sass/main.scss';
+import GeneratorBox from './GeneratorBox';
+import PasswordBox from './PasswordBox';
 
 const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+    <div className='generator'>
+      <PasswordBox />
+      <GeneratorBox />
     </div>
   );
 };

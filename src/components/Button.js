1bcdd9b802096df033fd3296bffcb7ee49b1e1bd
Clayton Dewey <clay@dryadmedia.com>
2023-01-14 13:43:39 -0600

    Create Button component

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import { Icon } from '../Icons';
+
+const Button = () => {
+  return (
+    <button type='button'>
+      Generate <Icon name='arrow-right' />
+    </button>
+  );
+};
+
+export default Button;

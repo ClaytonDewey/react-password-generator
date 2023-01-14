419b3363a2e65fc3e51e008111c71919e0ff94bf
Clayton Dewey <clay@dryadmedia.com>
2023-01-14 13:45:22 -0600

    Create box container for password settings

---
+++ src/components/GeneratorBox.js
@@ -0,0 +1,14 @@
+import StrengthBox from './StrengthBox';
+import Button from './Button';
+
+const GeneratorBox = () => {
+  return (
+    <div className='box__generator'>
+      <h1>Character Length</h1>
+      <StrengthBox />
+      <Button />
+    </div>
+  );
+};
+
+export default GeneratorBox;

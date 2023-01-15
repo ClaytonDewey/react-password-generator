bb32eb7316bbc23d9e9c5e6fb53e0e458791dcec
Clayton Dewey <clay@dryadmedia.com>
2023-01-15 17:16:28 -0600

    Create an indicators component

---
+++ src/components/Indicators.js
@@ -0,0 +1,13 @@
+const Indicators = () => {
+  return (
+    <span className='indicators uppercase'>
+      weak
+      <i className='indicator info'></i>
+      <i className='indicator info'></i>
+      <i className='indicator info'></i>
+      <i className='indicator blank'></i>
+    </span>
+  );
+};
+
+export default Indicators;

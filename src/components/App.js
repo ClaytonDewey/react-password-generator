f7f31cffebdaf475e2f3d4e190e92bc5bde09929
Clayton Dewey <clay@dryadmedia.com>
2023-01-15 14:58:38 -0600

    Set state & onChange function for pw length slider

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import '../sass/main.scss';
 import GeneratorBox from './GeneratorBox';
 import PasswordBox from './PasswordBox';
 
 const App = () => {
+  const [length, setLength] = useState(25);
+
+  const handleLengthChange = (e) => {
+    let target = e.target;
+
+    const min = target.min;
+    const max = target.max;
+    const val = target.value;
+
+    target.style.backgroundSize = ((val - min) * 100) / (max - min) + '% 100%';
+    setLength(val);
+  };
+
   return (
     <div className='generator'>
       <PasswordBox />
-      <GeneratorBox />
+      <GeneratorBox length={length} handleLengthChange={handleLengthChange} />
     </div>
   );
 };

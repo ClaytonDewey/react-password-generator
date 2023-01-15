3cdd050ede8ef9e4f852ed52cdac4109db336862
Clayton Dewey <clay@dryadmedia.com>
2023-01-15 14:59:26 -0600

    Add output for displaying password length

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,8 +1,21 @@
-const Slider = () => {
+const Slider = ({ length, handleLengthChange }) => {
+  const handleChange = (e) => {
+    handleLengthChange(e);
+  };
+
   return (
     <div className='slider'>
       <label htmlFor='length'>Character Length</label>
-      <input type='range' name='length' id='length' min='6' max='40' />
+      <input
+        type='range'
+        onChange={handleChange}
+        name='length'
+        id='length'
+        min='10'
+        max='40'
+        defaultValue={length}
+      />
+      <output id='rangevalue'>{length}</output>
     </div>
   );
 };

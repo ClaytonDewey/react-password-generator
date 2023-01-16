79c25715b547c70ec25b5cab7b44bdaeb850040d
ClaytonDewey <clay@dryadmedia.com>
2023-01-16 17:44:33 -0600

    Got rid of unnecessary 'blank' class

--- src/components/Indicators.js
+++ src/components/Indicators.js
@@ -1,11 +1,10 @@
 const Indicators = () => {
   return (
     <span className='indicators uppercase'>
-      weak
-      <i className='indicator info'></i>
-      <i className='indicator info'></i>
-      <i className='indicator info'></i>
-      <i className='indicator blank'></i>
+      <i className='indicator'></i>
+      <i className='indicator'></i>
+      <i className='indicator'></i>
+      <i className='indicator'></i>
     </span>
   );
 };

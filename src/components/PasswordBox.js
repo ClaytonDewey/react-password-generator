3946c691f58713a33b4b2da138b4380169e9a5a3
ClaytonDewey <clay@dryadmedia.com>
2023-01-17 10:57:33 -0600

    Show copied message on click

--- src/components/PasswordBox.js
+++ src/components/PasswordBox.js
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import { Icon } from '../Icons';
 
 const PasswordBox = ({ password }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(password);
+    setIsVisible(true);
+
+    setTimeout(() => {
+      setIsVisible(false);
+    }, 3000);
+  };
+
   return (
     <div className='box box__password'>
       <input type='text' defaultValue={password} placeholder='P4$$W0rD!' />
-      <button type='button'>
+      <button type='button' onClick={handleClick}>
         <Icon name='copy' />
       </button>
+      {isVisible && <span className='on-success-copied uppercase'>copied</span>}
     </div>
   );
 };

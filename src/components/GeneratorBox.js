38a11c510557ec7357965e0dc1eb68585029edcd
ClaytonDewey <clay@dryadmedia.com>
2023-01-16 18:38:03 -0600

    Pass necessary props down to children

--- src/components/GeneratorBox.js
+++ src/components/GeneratorBox.js
@@ -3,17 +3,31 @@ import Button from './Button';
 import Checkbox from './Checkbox';
 import Slider from './Slider';
 
-const GeneratorBox = ({ length, handleLengthChange }) => {
+const GeneratorBox = ({
+  length,
+  strength,
+  handleLengthChange,
+  onChangeUseUppercase,
+  onChangeUseLowercase,
+  onChangeUseNumbers,
+  onChangeUseSymbols,
+}) => {
   return (
     <div className='box box__generator'>
       <Slider length={length} handleLengthChange={handleLengthChange} />
       <div className='mb-3'>
-        <Checkbox label='Include Uppercase Letters' />
-        <Checkbox label='Include Lowercase Letters' />
-        <Checkbox label='Include Numbers' />
-        <Checkbox label='Include Symbols' />
+        <Checkbox
+          onChange={onChangeUseUppercase}
+          label='Include Uppercase Letters'
+        />
+        <Checkbox
+          onChange={onChangeUseLowercase}
+          label='Include Lowercase Letters'
+        />
+        <Checkbox onChange={onChangeUseNumbers} label='Include Numbers' />
+        <Checkbox onChange={onChangeUseSymbols} label='Include Symbols' />
       </div>
-      <StrengthBox />
+      <StrengthBox strength={strength} />
       <Button />
     </div>
   );

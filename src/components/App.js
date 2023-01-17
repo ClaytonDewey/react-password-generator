1ed335329fecdd5e8d5a5b50cc024fcc2cac5667
ClaytonDewey <clay@dryadmedia.com>
2023-01-17 10:14:24 -0600

    Create password generation function

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,14 @@ import PasswordBox from './PasswordBox';
 
 const App = () => {
   const [length, setLength] = useState(0);
-  const [password, setPassword] = useState('P4$$W0rD!');
+  const [password, setPassword] = useState('');
   const [strength, setStrength] = useState('');
-  let [useUppercase, setUseUppercase] = useState(false);
-  let [useLowercase, setUseLowercase] = useState(false);
-  let [useNumbers, setUseNumbers] = useState(false);
-  let [useSymbols, setUseSymbols] = useState(false);
+  const [activeItemsCount, setActiveItemsCount] = useState(0);
+  const [disabled, setDisabled] = useState(true);
+  const [useUppercase, setUseUppercase] = useState(false);
+  const [useLowercase, setUseLowercase] = useState(false);
+  const [useNumbers, setUseNumbers] = useState(false);
+  const [useSymbols, setUseSymbols] = useState(false);
 
   const handleLengthChange = (e) => {
     let target = e.target;
@@ -22,34 +24,64 @@ const App = () => {
   };
 
   const onChangeUseUppercase = () => {
-    setUseUppercase((useUppercase = !useUppercase));
+    setUseUppercase(!useUppercase);
   };
 
   const onChangeUseLowercase = () => {
-    setUseLowercase((useLowercase = !useLowercase));
+    setUseLowercase(!useLowercase);
   };
 
   const onChangeUseNumbers = () => {
-    setUseNumbers((useNumbers = !useNumbers));
+    setUseNumbers(!useNumbers);
   };
 
   const onChangeUseSymbols = () => {
-    setUseSymbols((useSymbols = !useSymbols));
+    setUseSymbols(!useSymbols);
   };
 
   useEffect(() => {
     if (length === 0) {
       setStrength('');
-    } else if (length >= 1 && length <= 5) {
+    } else if (
+      (length >= 1 && activeItemsCount === 0) ||
+      (length === 0 && activeItemsCount >= 1)
+    ) {
       setStrength('too weak!');
-    } else if (length > 5 && length <= 10) {
+      setDisabled(true);
+    } else if (length >= 1 && length <= 4 && activeItemsCount >= 1) {
+      setStrength('too weak!');
+      setDisabled(false);
+    } else if (length > 4 && length <= 8 && activeItemsCount >= 2) {
       setStrength('weak');
-    } else if (length > 10 && length <= 25) {
+      setDisabled(false);
+    } else if (length > 8 && length <= 15 && activeItemsCount >= 3) {
       setStrength('medium');
-    } else {
+      setDisabled(false);
+    } else if (length > 15 && activeItemsCount > 2) {
       setStrength('strong');
+      setDisabled(false);
     }
-  }, [length, strength]);
+  }, [password, length, activeItemsCount]);
+
+  const generatePassword = () => {
+    const upperCaseLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '1234567890';
+    const symbols = '!@#$%^&*+=-_';
+    let validChars = '';
+
+    if (useUppercase) validChars += upperCaseLetter;
+    if (useLowercase) validChars += letters;
+    if (useNumbers) validChars += numbers;
+    if (useSymbols) validChars += symbols;
+
+    let generatedPassword = '';
+    for (let i = 0; i < length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    setPassword(generatedPassword);
+  };
 
   return (
     <>
@@ -64,6 +96,9 @@ const App = () => {
           length={length}
           strength={strength}
           handleLengthChange={handleLengthChange}
+          setActiveItemsCount={setActiveItemsCount}
+          generatePassword={generatePassword}
+          disabled={disabled}
         />
       </main>
     </>

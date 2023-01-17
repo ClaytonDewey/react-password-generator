f4d3d27a267377ce5b0e76d7736138181ab3be67
ClaytonDewey <clay@dryadmedia.com>
2023-01-16 18:39:10 -0600

    Reformat input

--- src/components/PasswordBox.js
+++ src/components/PasswordBox.js
@@ -1,9 +1,9 @@
 import { Icon } from '../Icons';
 
-const PasswordBox = () => {
+const PasswordBox = ({ password }) => {
   return (
     <div className='box box__password'>
-      <input type='text' placeholder='P4$$W0rD!' />
+      <input type='text' defaultValue={password} />
       <button type='button'>
         <Icon name='copy' />
       </button>

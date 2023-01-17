bd31e51e148242933be1b8ec7d022e61d3d4dd99
ClaytonDewey <clay@dryadmedia.com>
2023-01-17 10:16:25 -0600

    Track checked status of checkboxes

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,8 +1,31 @@
-const Checkbox = ({ label, onChange }) => {
+import { useEffect, useState } from 'react';
+
+const Checkbox = ({ label, onChange, setActiveItemsCount }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const changehandler = () => {
+    onChange();
+    setIsActive(!isActive);
+  };
+
+  useEffect(() => {
+    if (!isActive) {
+      setActiveItemsCount((prevCount) => {
+        if (prevCount !== 0) {
+          return prevCount - 1;
+        }
+        return prevCount;
+      });
+    }
+
+    if (isActive) {
+      setActiveItemsCount((prevCount) => prevCount + 1);
+    }
+  }, [isActive, setActiveItemsCount]);
   return (
     <>
       <label className='form-control'>
-        <input onChange={onChange} type='checkbox' />
+        <input checked={isActive} onChange={changehandler} type='checkbox' />
         {label}
       </label>
     </>

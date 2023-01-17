8a53899a6114bea93790814229be6295d1861a53
ClaytonDewey <clay@dryadmedia.com>
2023-01-17 10:17:08 -0600

    Update password input

--- src/components/PasswordBox.js
+++ src/components/PasswordBox.js
@@ -3,7 +3,7 @@ import { Icon } from '../Icons';
 const PasswordBox = ({ password }) => {
   return (
     <div className='box box__password'>
-      <input type='text' defaultValue={password} />
+      <input type='text' defaultValue={password} placeholder='P4$$W0rD!' />
       <button type='button'>
         <Icon name='copy' />
       </button>
